e009571021ef777cea3b44e28c614db0f70fd4c9
NewellClark <newellofthefuture@gmail.com>
2017-01-03 15:04:08 -0800

    Added project for web api (front and back end)

---
+++ src/WebApi/wwwroot/ngApp/app.ts
@@ -0,0 +1,85 @@
+namespace WebApi {
+
+    angular.module('WebApi', ['ui.router', 'ngResource', 'ui.bootstrap']).config((
+        $stateProvider: ng.ui.IStateProvider,
+        $urlRouterProvider: ng.ui.IUrlRouterProvider,
+        $locationProvider: ng.ILocationProvider
+    ) => {
+        // Define routes
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: '/ngApp/views/home.html',
+                controller: WebApi.Controllers.HomeController,
+                controllerAs: 'controller'
+            })
+            .state('secret', {
+                url: '/secret',
+                templateUrl: '/ngApp/views/secret.html',
+                controller: WebApi.Controllers.SecretController,
+                controllerAs: 'controller'
+            })
+            .state('login', {
+                url: '/login',
+                templateUrl: '/ngApp/views/login.html',
+                controller: WebApi.Controllers.LoginController,
+                controllerAs: 'controller'
+            })
+            .state('register', {
+                url: '/register',
+                templateUrl: '/ngApp/views/register.html',
+                controller: WebApi.Controllers.RegisterController,
+                controllerAs: 'controller'
+            })
+            .state('externalRegister', {
+                url: '/externalRegister',
+                templateUrl: '/ngApp/views/externalRegister.html',
+                controller: WebApi.Controllers.ExternalRegisterController,
+                controllerAs: 'controller'
+            }) 
+            .state('about', {
+                url: '/about',
+                templateUrl: '/ngApp/views/about.html',
+                controller: WebApi.Controllers.AboutController,
+                controllerAs: 'controller'
+            })
+            .state('notFound', {
+                url: '/notFound',
+                templateUrl: '/ngApp/views/notFound.html'
+            });
+
+        // Handle request for non-existent route
+        $urlRouterProvider.otherwise('/notFound');
+
+        // Enable HTML5 navigation
+        $locationProvider.html5Mode(true);
+    });
+
+    
+    angular.module('WebApi').factory('authInterceptor', (
+        $q: ng.IQService,
+        $window: ng.IWindowService,
+        $location: ng.ILocationService
+    ) =>
+        ({
+            request: function (config) {
+                config.headers = config.headers || {};
+                config.headers['X-Requested-With'] = 'XMLHttpRequest';
+                return config;
+            },
+            responseError: function (rejection) {
+                if (rejection.status === 401 || rejection.status === 403) {
+                    $location.path('/login');
+                }
+                return $q.reject(rejection);
+            }
+        })
+    );
+
+    angular.module('WebApi').config(function ($httpProvider) {
+        $httpProvider.interceptors.push('authInterceptor');
+    });
+
+    
+
+}
