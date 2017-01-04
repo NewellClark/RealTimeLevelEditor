a531ccea56a551e78a5260032630f0b22fd33202
NewellClark <newellofthefuture@gmail.com>
2017-01-03 17:23:20 -0800

    Added TileIndex struct. Added Newtonsoft JSON serialization package to RealTimeLevelEditor project

---
+++ src/LevelModelTests/TileIndexTests.cs
@@ -0,0 +1,37 @@
+﻿using RealTimeLevelEditor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LevelModelTests
+{
+	public class TileIndexTests
+	{
+		[Fact]
+		internal void EqualsOperator_ReturnsTrueWhenEqual()
+		{
+			long x = 12345;
+			long y = 98764234;
+			var tile1 = new TileIndex(x, y);
+			var tile2 = new TileIndex(x, y);
+
+			Assert.True(tile1 == tile2);
+			Assert.False(tile1 != tile2);
+		}
+
+		[Fact]
+		internal void EqualsOperator_ReturnsFalseWhenNotEqual()
+		{
+			long x = -32512;
+			long y = 987987;
+			var tile1 = new TileIndex(x, y);
+			var tile2 = new TileIndex(x + 1, y);
+			var tile3 = new TileIndex(x, y + 1);
+
+			Assert.False(tile1 == tile2);
+			Assert.False(tile1 == tile3);
+		}
+	}
+}
---
+++ src/RealTimeLevelEditor/TileIndex.cs
@@ -0,0 +1,42 @@
+﻿using MiscHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealTimeLevelEditor
+{
+	/// <summary>
+	/// Stores the absolute location of a tile within a level.
+	/// </summary>
+	public struct TileIndex
+	{
+		public TileIndex(long x, long y)
+		{
+			X = x;
+			Y = y;
+		}
+
+		public long X { get; }
+		public long Y { get; }
+
+		public override int GetHashCode()
+		{
+			return Hashing.StartingPrime
+				.CombineHashCodes(X)
+				.CombineHashCodes(Y);
+		}
+		public override bool Equals(object obj)
+		{
+			var casted = obj as TileIndex?;
+			return casted == this;
+		}
+
+		public static bool operator==(TileIndex lhs, TileIndex rhs)
+		{
+			return lhs.X == rhs.X &&
+				lhs.Y == rhs.Y;
+		}
+		public static bool operator!=(TileIndex lhs, TileIndex rhs) => !(lhs == rhs);
+	}
+}
