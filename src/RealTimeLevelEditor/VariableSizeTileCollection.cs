3440d7f628eeb8604087a23cca9083bc99cd9ef4
NewellClark <newellofthefuture@gmail.com>
2017-01-05 09:53:23 -0800

    Added unit tests to make sure FixedSizeTileCollection<T>.AddOrUpdate() and VariableSizeTileCollection<T>.AddOrUpdate() properly update existing tiles.

--- src/RealTimeLevelEditor/VariableSizeTileCollection.cs
+++ src/RealTimeLevelEditor/VariableSizeTileCollection.cs
@@ -7,40 +7,82 @@ namespace RealTimeLevelEditor
 {
 	public class VariableSizeTileCollection<T> : TileCollection<T>
 	{
+		public VariableSizeTileCollection() 
+			: this(new Tile<T>[] { }) { }
 		public VariableSizeTileCollection(IEnumerable<Tile<T>> tiles)
 		{
 			_tiles = new Dictionary<TileIndex, Tile<T>>();
-			
+			AddOrUpdateNonVirtual(tiles);
 		}
-
+		
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="index"></param>
+		/// <returns></returns>
+		/// <exception cref="KeyNotFoundException">No tile with the specified index
+		/// in the current collection.</exception>
 		public override Tile<T> this[TileIndex index]
 		{
 			get
 			{
-				throw new NotImplementedException();
+				return _tiles[index];
 			}
 		}
 
+		/// <summary>
+		/// If a tile with the specified index already exists, it is updated to the 
+		/// specified value. If not, the specified tile is added to the collection.
+		/// </summary>
+		/// <param name="tile"></param>
 		public override void AddOrUpdate(Tile<T> tile)
 		{
-			throw new NotImplementedException();
+			AddOrUpdateNonVirtual(tile);
 		}
 
+		/// <summary>
+		/// Indicates whether there is a tile in the current collection with the 
+		/// specified index.
+		/// </summary>
+		/// <param name="index"></param>
+		/// <returns></returns>
 		public override bool Contains(TileIndex index)
 		{
-			throw new NotImplementedException();
+			return _tiles.ContainsKey(index);
 		}
 
+		/// <summary>
+		/// If there is a tile with the specified index in the current collection,
+		/// it is deleted. If there is no tile with the specified index, nothing happens.
+		/// </summary>
+		/// <param name="index"></param>
 		public override void Delete(TileIndex index)
 		{
-			throw new NotImplementedException();
+			_tiles.Remove(index);
 		}
 
 		public override IEnumerator<Tile<T>> GetEnumerator()
 		{
-			throw new NotImplementedException();
+			foreach (var tile in _tiles.Values)
+			{
+				yield return tile;
+			}
 		}
 
+		/// <summary>
+		/// It's bad practice to call a virtual method in a constructor, so 
+		/// putting the body of AddOrUpdate() in here. 
+		/// </summary>
+		/// <param name="tile"></param>
+		private void AddOrUpdateNonVirtual(Tile<T> tile)
+		{
+			_tiles[tile.Index] = tile;
+		}
+		private void AddOrUpdateNonVirtual(IEnumerable<Tile<T>> tiles)
+		{
+			foreach (var tile in tiles)
+				AddOrUpdateNonVirtual(tile);
+		}
 
 		private Dictionary<TileIndex, Tile<T>> _tiles;
 	}
