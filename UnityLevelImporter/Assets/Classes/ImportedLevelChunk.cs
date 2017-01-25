08b784af98cf4ca4a7b34d28bb06a75de59dab43
NewellClark <newellofthefuture@gmail.com>
2017-01-24 21:21:45 -0800

    Added a unit-testing project.

---
+++ UnityLevelImporter/Assets/Classes/ImportedLevelChunk.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace UnityLevelImporter
+{
+	[Serializable]
+	class ImportedLevelChunk
+	{
+		[JsonProperty]
+		public Rectangle Region { get; private set; }
+
+		[JsonProperty]
+		public Tile[] Tiles { get; private set; }
+	}
+
+	[Serializable]
+	class Tile
+	{
+		public Tile() { }
+
+		public Tile(TileIndex index, string data)
+		{
+			Index = index;
+			Data = data;
+		}
+
+		[JsonProperty]
+		public TileIndex Index { get; private set; }
+
+		[JsonProperty]
+		public string Data { get; set; }
+	}
+}
