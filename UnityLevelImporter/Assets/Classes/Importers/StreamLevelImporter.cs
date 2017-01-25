67a5b391b1d05a3373aabfa067d75e2479ecc027
NewellClark <newellofthefuture@gmail.com>
2017-01-25 14:30:11 -0800

    Added LevelExporter class to LevelModel project.

---
+++ UnityLevelImporter/Assets/Classes/Importers/StreamLevelImporter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace UnityLevelImporter
+{
+	/// <summary>
+	/// Reads a level from a <c>Stream</c>
+	/// </summary>
+	public abstract class StreamLevelImporter : LevelImporter
+	{
+		protected override TextReader GetTextReader()
+		{
+			return new StreamReader(GetStream());
+		}
+
+		protected abstract Stream GetStream();
+	}
+}
---
+++ src/RealTimeLevelEditor/LevelExporter.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealTimeLevelEditor
+{
+	/// <summary>
+	/// Serializes <c>Level`T'</c> into a format that is compatible with various external tools.
+	/// </summary>
+	public class LevelExporter
+	{
+		private class ExportableLevel<T>
+		{
+			public ExportableLevel() { }
+			public ExportableLevel(IEnumerable<LevelChunk<T>> chunks)
+			{
+				Chunks = chunks.ToArray();
+			}
+
+			[JsonProperty]
+			public LevelChunk<T>[] Chunks { get; set; }
+		}
+
+		/// <summary>
+		/// Exports the specified level to a JSON format that is compatible with various external tools.
+		/// </summary>
+		/// <typeparam name="T">Tile data type.</typeparam>
+		/// <param name="textWriter">The <c>TextWriter</c> that the serialized form of the level will
+		/// be written to.</param>
+		/// <param name="level">The level to export.</param>
+		public void Export<T>(TextWriter textWriter, Level<T> level)
+		{
+			var chunks = level.GetChunks()
+				.Select(x => x.Data)
+				.ToArray();
+			var exportable = new ExportableLevel<T>(chunks);
+
+			var serializer = new JsonSerializer();
+			serializer.Serialize(textWriter, exportable);
+		}
+	}
+}
