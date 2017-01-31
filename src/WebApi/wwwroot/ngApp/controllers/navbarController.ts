61fc28636dfb0c7868190b30256e371e81f0ac68
NewellClark <newellofthefuture@gmail.com>
2017-01-31 12:10:21 -0800

    Changed NavbarAction so it uses IStateService instead of ILocationService to navigate.

--- src/WebApi/wwwroot/ngApp/controllers/navbarController.ts
+++ src/WebApi/wwwroot/ngApp/controllers/navbarController.ts
@@ -1,17 +1,29 @@
 ﻿namespace WebApi.Controllers {
 
+	class ActionArgs {
+		constructor(
+			public readonly $location: ng.ILocationService,
+			public readonly $state: ng.ui.IStateService) { }
+	}
 	export class NavbarAction {
 		public constructor(
 			private _displayName: string,
 			public readonly path: string,
-			protected $location: ng.ILocationService) { }
+			args: ActionArgs) {
+			this.$location = args.$location;
+			this.$state = args.$state;
+		}
+
+		protected readonly $location: ng.ILocationService;
+		protected readonly $state: ng.ui.IStateService;
 
 		public isVisible(account: AccountController): boolean {
 			return true;
 		}
 
 		public execute(account: AccountController): void {
-			this.$location.path(this.path);
+			//this.$location.path(this.path);
+			this.$state.go(this.path);
 		}
 
 		public displayName(): string {
@@ -42,19 +54,27 @@
 	class LogoutAction extends RequiresLoggedInAction {
 		public execute(account: AccountController): void {
 			account.logout();
+			super.execute(account);
 		}
 	}
 
 	export class NavbarController {
 		constructor(
 			private $location: ng.ILocationService,
-			private $http: ng.IHttpService) {
+			private $http: ng.IHttpService,
+		private $state: ng.ui.IStateService) {
 			this.actions = [];
-			this.home = this.addAction(new NavbarAction("Home", "/", $location));
-			this.projects = this.addAction(new RequiresLoggedInAction("Projects", "/projects", $location));
-			this.logout = this.addAction(new LogoutAction("Logout", this.home.path, $location));
-			this.login = this.addAction(new RequiresLoggedOutAction("Login", this.home.path, $location));
-			this.register = this.addAction(new RequiresLoggedOutAction("Register", "/register", $location));
+			//this.home = this.addAction(new NavbarAction("Home", "/", $location));
+			//this.projects = this.addAction(new RequiresLoggedInAction("Projects", "/projects", $location));
+			//this.logout = this.addAction(new LogoutAction("Logout", this.home.path, $location));
+			//this.login = this.addAction(new RequiresLoggedOutAction("Login", this.home.path, $location));
+			//this.register = this.addAction(new RequiresLoggedOutAction("Register", "/register", $location));
+			let args = new ActionArgs($location, $state);
+			this.home = this.addAction(new NavbarAction("Home", "home", args));
+			this.projects = this.addAction(new RequiresLoggedInAction("Projects", "projects", args));
+			this.login = this.addAction(new RequiresLoggedOutAction("Login", "login", args));
+			this.logout = this.addAction(new LogoutAction("Logout", this.home.path, args));
+			this.register = this.addAction(new RequiresLoggedOutAction("Register", "register", args));
 		}
 
 		public readonly actions: NavbarAction[];
@@ -70,10 +90,6 @@
 		public readonly logout: NavbarAction;
 
 		public readonly showDisabledLinks: boolean = true;
-
-		public isVisible(): boolean {
-
-		}
 
 		private addAction(action: NavbarAction): NavbarAction {
 			this.actions.push(action);
