286ab88ab91767f5983703ba9e093cc3b8edd6f4
NewellClark <newellofthefuture@gmail.com>
2017-01-31 14:15:56 -0800

    ALready accepted this

--- src/WebApi/wwwroot/ngApp/controllers/levelAdminController.ts
+++ src/WebApi/wwwroot/ngApp/controllers/levelAdminController.ts
@@ -20,14 +20,14 @@
 			});
 		}
 
-	public editLevel(index) {
-		//localStorage.setItem("levelId", this.levelRoster[index].levelId);
-		//localStorage.setItem("levelName", this.levelRoster[index].name);
-		//this.$location.path('/level');
-		let params = {
-			id: this.levelRoster[index].levelId
-		}
-		this.$state.go(States.level, params);
+		public editLevel(index) {
+			localStorage.setItem("levelId", this.levelRoster[index].levelId);
+			localStorage.setItem("levelName", this.levelRoster[index].name);
+			//this.$location.path('/level');
+			let params = {
+				id: this.levelRoster[index].levelId
+			}
+			this.$state.go(States.level, params);
 
 
 		}
@@ -49,13 +49,23 @@
 			let levelId = this.levelRoster[index].levelId;
 			let url = `/api/levels/${levelId}/download`;
 
-	constructor(
-		private $http: ng.IHttpService,
-		private $location: ng.ILocationService,
-		private $state: ng.ui.IStateService,
-		private $stateParams: ng.ui.IStateParamsService) {
-		this.projectId = localStorage.getItem("projectId");
-		this.projectName = localStorage.getItem("projectName");
-		this.getLevels();
+			this.$location.path(url);
+			let element = document.createElement("a");
+			element.setAttribute("href", url);
+			element.setAttribute("target", "blank");
+			element.click();
+		}
+
+		constructor(
+			private $http: ng.IHttpService,
+			private $location: ng.ILocationService,
+			private $state: ng.ui.IStateService,
+			private $stateParams: ng.ui.IStateParamsService) {
+			//this.projectId = localStorage.getItem("projectId");
+			//this.projectName = localStorage.getItem("projectName");
+
+			this.getLevels();
+		}
+
 	}
 }
